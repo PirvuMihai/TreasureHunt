c93131df4f169a37c4dae090da078ff7c9dcb586
mihai <mihai17pirvu2003@gmail.com>
2025-11-09 14:15:45 +0200

    a

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -1,30 +1,29 @@
-let user_controller = {
-
-	// user object, register/login mode
-	function validate_user_info(user, mode) {
-		if (mode == 'login') {
-			for (let u in users) {
-				if (user.name == u.name && hash(user.password) == u.password)
-					return u
-			}
-			return false
-		} else if (mode == 'register') {
-			if (u.password.length < 8 
-				|| !!(new Date(u.date_of_birth) 
-				|| !u.first_name 
-				|| !u.last_name 
-				|| !u.email))
-				return false
-			return u
-		} else {
-			pr('No such validation mode.')
+// user object, register/login mode
+function validate_user_info(user, mode) {
+	if (mode == 'login') {
+		for (let u in users) {
+			if (user.name == u.name && hash(user.password) == u.password)
+				return u
 		}
+		return false
+	} else if (mode == 'register') {
+		if (u.password.length < 8 
+			|| !!(new Date(u.date_of_birth) 
+			|| !u.first_name 
+			|| !u.last_name 
+			|| !u.email))
+			return false
+		return u
+	} else {
+		pr('No such validation mode.')
 	}
+}
 
+let user_controller = {
 	register = async function(req, res) {
 		try {
 			let user = validate_users_info(req.body, 'register')
-			
+
 		} catch(e) {
 
 		}
